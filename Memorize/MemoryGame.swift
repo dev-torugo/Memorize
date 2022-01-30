6ce55a5bf83e9ea4f5f230087857e580fe8d184c
dev-torugo <victorhacmo@gmail.com>
2022-01-30 11:44:36 -0300

    code att

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -13,19 +13,9 @@ struct MemoryGame<CardContent> where CardContent: Equatable { //<> to show the w
     
     private var indexOfTheOneAndOnlyFaceUpCard: Int? { // we gonna compute it from line 12 -> cards: Array<Card> by looking them all
         get{
-            var faceUpCardIndices = [Int]()
-            for index in cards.indices {
-                if cards[index].isFaceUp{
-                    faceUpCardIndices.append(index)
-                }
-            }
-            if faceUpCardIndices.count == 1 {
-                    return faceUpCardIndices.first
-            } else {
-                    return nil
-                }
-            }
-
+            let faceUpCardIndices = cards.indices.filter({cards [$0].isFaceUp})
+            return faceUpCardIndices.oneAndOnly
+        }
         set {
             for index in cards.indices{ // 0..<cards.count foi substituido por cards.indices
                 if index != newValue {
@@ -59,22 +49,32 @@ struct MemoryGame<CardContent> where CardContent: Equatable { //<> to show the w
         
     }
     //        print("\(cards)")
-
-
+    
+    
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {  // createCardContent now is a argument for this function
         cards = [] // faz com que cards seja uma array vazia -> Array<Card>() pode ser substituido por [] pois o Swift ja sabe que cards é uma array (linha 12)
         // add the number of pairs x 2 cards to cards
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex) // a função createCardContent will be responsible to create the content of the cards and has the pairIndex as parameter
-                cards.append(Card(content: content, id: pairIndex*2))
-                cards.append(Card(content: content, id: pairIndex*2+1))
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
         }
     }
-
+    
     struct Card: Identifiable {
         var isFaceUp = false // Bool = false it isn't necessary
         var isMatched = false // Bool = false it isn't necessary
         let content: CardContent // a don't care type
         let id: Int
     }
+}
+
+extension Array{
+    var oneAndOnly: Element? { // array dont care with optional
+        if count == 1{
+            return  first
+        } else {
+            return nil
+        }
     }
+}
